0598cac0fe4a220e025162821d9bc1c6092deaaa
Allen Mashiku <mashikuallen@gmail.com>
2026-04-03 13:54:14 +0300

    added a production url

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002',
+  baseURL: 'https://ecards.mashikutech.co.tz/digitalinvitations',
   timeout: 10000, // 10 seconds
 });
 
